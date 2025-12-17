6a9117ff54d2d433ee449d98b833a41483951c8b
ErenGlds <ythefeeder@gmail.com>
2025-12-17 11:56:35 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -8,23 +8,18 @@ fetchStudents(rawData => {
   );
   console.log("Testing Immutability:");
   console.log("Original ID:", students[0].id);
-  console.log("Attempting to change ID...");
-  students[0].id = 103;
-  console.log(
-    "Final ID:",
-    students[0].id,
-    "(should be unchanged)"
-  );
-
+  
   console.log("Student Averages:");
 
   const avg101 = calculateClassAverage(students, 101);
   console.log("Class Average for Course 101:", avg101);
 
-  const topStudent = findTopStudent(students);
-  console.log("Top Student: ${topStudent.name} (Average: ${topStudent.getAverage()})");
+  const topStudent = findTopStudent(students);console.log(`Top Student: ${topStudent.name} (Average: ${topStudent.getAverage()})`);
 
   const course102Students = filterStudents(students,s => s.courses.some(c => c.courseId === 102));
 
   console.log("Students in Course 102:",course102Students.map(s => s.name).join(", "));
-});
+
+  console.log("Attempting to change ID...");
+  students[0].id = 103;// Can't be changed
+  });
